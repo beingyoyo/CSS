6b730e569e6dc72fd9cf49f51c7468a10ccf7cdc
Idrees <idrees.d@somaiya.edu>
2019-02-13 14:48:41 +0530

    alpha

--- diiffehellman.c
+++ diiffehellman.c
@@ -1,8 +1,37 @@
 #include<stdio.h>
+#include <math.h>
+#include <stdint.h>
 void main()
 {
 	int q=11; //prime number is must
-	double alpha; //find alpha, such that it is a primitive root of q
+	int alpha; 
+	int x,y=1; 
+	uint8_t table[q-1];
+
+	//finding alpha
+	for (int i = 2; i < (q); i++)
+	{
+		x=i;
+		for(int j=2;j<q;j++)
+		{
+			x=(x*y)%q;
+			if (table[x]==true)
+			{
+				continue;
+			}
+			else
+			{
+				table[x]=true;
+				y=x;
+			}
+		}
+		if(j==q)
+		{
+			alpha = i;
+			break;
+		}
+	}
+
 
 
 }
