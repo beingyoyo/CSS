b60f5b8c9ae36727bd7341e4764bd8707a08e3b4
beingyoyo <sanjaymakwana02@gmail.com>
2019-05-01 22:18:50 +0530

    S-box

--- sbox.c
+++ sbox.c
@@ -3,24 +3,30 @@
 int binToDec(char *arr, int size){
 	int i;
 	int val = 0;
-	for(i=0;i<size;i++){
-		if(arr[i] & 1){
-			val += 1<<size-i-1;
+	for(i=0;i<size;i++)
+	{
+		if(arr[i] & 1)
+		{
+			val += 1<<(size-i-1);
 		}
 	}
 
 	return val;
 }
 
-void decToBin(int x){
+void decToBin(int x)
+{
 	int i;
 	x = (char)x;
 	char mask = 8;
-	for(i=0;i<4;i++){
-		if(mask & x){
+	for(i=0;i<4;i++)
+	{
+		if(mask & x)
+		{
 			printf("1");
 		}
-		else{
+		else
+		{
 			printf("0");
 		}
 		mask = mask >> 1;
@@ -28,7 +34,8 @@ void decToBin(int x){
 	printf("\n");
 }
 
-void main(){
+void main()
+{
 	int sbox[4][16] = {{14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7},
 						{0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8},
 						{4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0},
@@ -41,10 +48,10 @@ void main(){
 
 	scanf("%d",&input);
 	ch = (char)input;
-	// printf("\n%d",ch);
+	printf("\n%d",ch);
 
 	ch = ch & 0x3f;
-	// printf("\n%d",ch);
+	printf("\n%d",ch);
 
 	row[0] = (char)(ch & 1<<5)>>5;
 	row[1] = (char)ch & 1;
@@ -56,6 +63,6 @@ void main(){
 	r = binToDec(row,2);
 	c = binToDec(col,4);
 
-	// printf("\nr:%d  c:%d\n",r,c);
+	printf("\nr:%d  c:%d\n",r,c);
 	decToBin(sbox[r][c]);
 }
