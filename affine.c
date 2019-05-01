180abce260421f88ca0bf1728fca5fd10c0b838a
beingyoyo <sanjaymakwana02@gmail.com>
2019-05-01 21:25:09 +0530

    Affine

--- affine.c
+++ affine.c
@@ -7,8 +7,10 @@
 
 int gcd(int a, int m){
 	int temp;
-	while(a != 0){
-		if(a == 1){
+	while(a != 0)
+	{
+		if(a == 1)
+		{
 			return 1;
 		}
 		temp = a;
@@ -19,22 +21,26 @@ int gcd(int a, int m){
 	return 0;
 }
 
-int additiveInv(int k){
-	if(k < 26){
+int additiveInv(int k)
+{
+	if(k < 26)
+	{
 		return abs(k-26);
 	}
 	return abs(k%26 - 26);
 }
 
 //extended Euclidean theorem
-int mulInv(int k){
+int mulInv(int k)
+{
 	int t = 0;
 	int newt = 1;
 	int r = 26;
 	int newr = k;
 	int temp1,temp2,q;
 
-	while(newr != 0){
+	while(newr != 0)
+	{
 		q = r/newr;
 		temp1 = newt;
 		temp2 = t - q*newt;
@@ -46,7 +52,8 @@ int mulInv(int k){
 		newr = temp2;
 	}
 
-	if(t < 0){
+	if(t < 0)
+	{
 		t = t+26;
 	}
 
@@ -61,7 +68,8 @@ int mulInv(int k){
 // 	return k;
 // }
 
-void decrypt(char *str, int k1, int k2){
+void decrypt(char *str, int k1, int k2)
+{
 	int i;
 	char ch;
 	k2 = additiveInv(k2);
@@ -69,7 +77,8 @@ void decrypt(char *str, int k1, int k2){
 
 	printf("\nDecrypted string: ");
 
-	for(i=0;i<strlen(str);i++){
+	for(i=0;i<strlen(str);i++)
+	{
 		ch = str[i];
 		ch = ch - 'a';
 		// printf("\n%d\n",ch);
@@ -79,7 +88,8 @@ void decrypt(char *str, int k1, int k2){
 	printf("\n");
 }
 
-void encrypt(char *str, int k1, int k2){
+void encrypt(char *str, int k1, int k2)
+{
 	int i;
 	char ch;
 	char encrypted_string[strlen(str)];
@@ -96,7 +106,8 @@ void encrypt(char *str, int k1, int k2){
 	decrypt(encrypted_string,k1,k2);
 }
 
-void main(){
+void main()
+{
 	int k1,k2;
 	char *input;
 	int val = 0;
@@ -104,17 +115,21 @@ void main(){
 	printf("Enter string to be encrypted: ");
 	scanf("%s",input);
 
-	while(!val){
+	while(!val)
+	{
 		printf("\nEnter key 1: ");
 		scanf("%d",&k1);	
-		if(k1 > 26){
+		if(k1 > 26)
+		{
 			val = gcd(26,k1);	
 		}
-		else{
+		else
+		{
 			val = gcd(k1,26);
 		}
 
-		if(val != 1){
+		if(val != 1)
+		{
 			printf("\nInverse does not exist. Enter another key.");
 		}
 	}
